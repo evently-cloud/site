62e28d64f4afa89667d3a629e8655877ab890db4
BeckyPollard <hello@beckypollard.com>
2022-11-04 16:44:49 -0400

    Add support for images in blog folders, jpg and png

--- .eleventy.js
+++ .eleventy.js
@@ -34,6 +34,8 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ 'assets/favicon.ico': '/favicon.ico' });
     eleventyConfig.addWatchTarget('assets/*.json');
     eleventyConfig.addPassthroughCopy('assets/*.json');
+    eleventyConfig.addPassthroughCopy('src/blog/*/*.jpg');
+    eleventyConfig.addPassthroughCopy('src/blog/*/*.png');
 
     eleventyConfig.addShortcode('shinyAJSFunc', function () {
         return "@mousemove=\"$el.style.setProperty('--x', $event.clientX - $el.getBoundingClientRect().x);$el.style.setProperty('--y', $event.clientY - $el.getBoundingClientRect().y)\"";
